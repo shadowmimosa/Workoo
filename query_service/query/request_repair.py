702b0e5a19f463c414307e1a4b557532482a9290
fanwantai <fwt1575272832@163.com>
2019-05-02 17:23:58 +0800

    当 401 时，初始化请求

--- query_service/query/request_repair.py
+++ query_service/query/request_repair.py
@@ -20,8 +20,7 @@ class RepairSalt(object):
         self.header = {
             "Host":
             "repair.salt.ch",
-            "Accept":
-            "application/json, text/javascript, */*; q=0.01",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
             "X-Requested-With":
             "XMLHttpRequest",
             # "Connection": "keep-alive",
@@ -42,7 +41,7 @@ class RepairSalt(object):
 
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-        #设置重连次数
+        # 设置重连次数
         requests.adapters.DEFAULT_RETRIES = 5
         # 设置连接活跃状态为False
         session = requests.session()
@@ -50,7 +49,7 @@ class RepairSalt(object):
         session.verify = False
 
         adapter = requests.adapters.HTTPAdapter(max_retries=3)
-        #将重试规则挂载到http和https请求
+        # 将重试规则挂载到http和https请求
         session.mount('http://', adapter)
         session.mount('https://', adapter)
 
@@ -123,6 +122,10 @@ class RepairSalt(object):
                 return resp.content
             else:
                 return resp.text
+        elif resp.status_code == 401:
+            self.init_request()
+            print("---> Retrying because 401")
+            self.deal_re(byte=byte, url=url, header=header)
         else:
             print("---> {} 请求失败！状态码为{}，共耗时{:.3}秒\n".format(
                 url, resp.status_code, end_time - start_time))
@@ -133,7 +136,7 @@ class RepairSalt(object):
         )
         filter_list = []
         judge_list = re.findall(pattern, content)
-        if len(judge_list)==0 and "FMiPText" in content:
+        if len(judge_list) == 0 and "FMiPText" in content:
             return None
 
         for value in judge_list:
@@ -143,6 +146,23 @@ class RepairSalt(object):
             filter_list.append(value)
         print(filter_list)
         return filter_list
+
+    def init_request(self):
+        url = "http://repair.salt.ch/CaseWizard/Wizard/27427839-ade1-46ad-ab14-e03bcdf4b5b4/Salt/RepairAtHome"
+        header = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Cookie": "session.repairline=4pdwfw0xn1lxi4ezmz3wbokv; settings.repairline=pu4IokItfq5LEWk70NhlOQ==",
+            "Host": "repair.salt.ch",
+            "Pragma": "no-cache",
+            "Upgrade-Insecure-ReUpgradequests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
+
+        }
+        resp = self.deal_re(url=url, header=header)
 
     def run(self, imei):
         url = self.url.format(imei, "1849673")
@@ -170,4 +190,5 @@ def main(imei="356726086774842"):
 if __name__ == "__main__":
     import os
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    main()
+    # main()
+    RepairSalt().init_request()
