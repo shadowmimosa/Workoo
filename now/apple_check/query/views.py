c64fa6412be01220ed30b8300c5b82d9c1024bdb
shadowmimosa <chexuandong@foxmail.com>
2019-06-26 20:50:04 +0800

    modify snError

--- now/apple_check/query/views.py
+++ now/apple_check/query/views.py
@@ -15,30 +15,28 @@ def query(request):
             if imei:
                 start_time = time.time()
                 query_data = request_repair.main(imei)
+                magic_time = time.time() - start_time
+
                 if query_data == "snError":
                     json_ = {
                         "status": "success",
                         # "imei":imei,
                         "msg": "序列号已更换"
                     }
-                    return HttpResponse(
-                        json.dumps(json_, ensure_ascii=False),
-                        content_type="application/json;charset=utf-8")
-                    
                 elif isinstance(query_data, dict):
-                    magic_time = time.time() - start_time
-                    if magic_time < 5:
-                        time.sleep(5 - magic_time)
                     json_ = {
                         "status": "success",
                         # "imei":imei,
                         "msg": query_data
                     }
-                    return HttpResponse(
-                        json.dumps(json_, ensure_ascii=False),
-                        content_type="application/json;charset=utf-8")
                 else:
                     return HttpResponse("403 error")
+
+                if magic_time < 5:
+                    time.sleep(5 - magic_time)
+                return HttpResponse(
+                    json.dumps(json_, ensure_ascii=False),
+                    content_type="application/json;charset=utf-8")
             else:
                 return HttpResponse("您的输入不合法，请重新输入")
         except Exception as exc:
