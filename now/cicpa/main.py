67e51ac178209e3bbf5b2e0bd509b14f4f1523af
shadowmimosa <chexuandong@foxmail.com>
2019-09-19 16:40:02 +0800

    221

--- now/cicpa/main.py
+++ now/cicpa/main.py
@@ -263,7 +263,7 @@ class DealCicpa(object):
         # for page in range(2, 700):
         #     print("--->Info: Office page is {}".format(page))
         #     self.get_office_list(page)
-        for page in range(48, 91):
+        for page in range(211, 221):
             print("--->Info: Office page is {}".format(page))
             try:
                 self.get_office_list(page)
