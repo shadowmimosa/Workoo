e7ba921710ee9746efdb3fe48a812c3d7ed403da
shadowmimosa <chexuandong@foxmail.com>
2019-11-06 22:19:19 +0800

    almost finish

--- now/ichengyun/auto.py
+++ now/ichengyun/auto.py
@@ -1,8 +1,10 @@
 import io
 import time
+import pandas
 from PIL import ImageGrab
 import win32gui, win32api, win32con
 
+from utils.facepp import FaceOcr
 from utils.baidu_ocr import BaiduOCR
 
 
@@ -36,29 +38,121 @@ class WinOperate(object):
 class Ichengyun(object):
     def __init__(self):
         self.win = WinOperate()
+        self.info = pandas.DataFrame(columns=["姓名", "注册号", "证书", "电话"])
+        # df.drop(df.index,inplace=True)
 
     def get_word(self):
         pic = self.win.screenshot()
         byte_io = io.BytesIO()
         pic.save(byte_io, 'png')
         byte_io.seek(0)
-        a = BaiduOCR().pic2word(byte_io.read())
-        print(a['words_result'])
+
+        words = BaiduOCR().pic2word(byte_io.read())["words_result"]
+        # words = FaceOcr().pic2word(byte_io.read())["result"]
+        print(words)
+
+        if words:
+            self.data_clean(words)
+
+    def data_clean(self, words):
+        info = {}
+        register_list = []
+        for item in words:
+            word = item["words"]
+
+            if ":" in word:
+                temp = word.split(":")
+                if "姓名" in temp[0]:
+                    if "(" in temp[1]:
+                        info["姓名"] = temp[1].split("(")[0]
+                    elif "（" in temp[1]:
+                        info["姓名"] = temp[1].split("（")[0]
+                    elif "O" in temp[1]:
+                        info["姓名"] = temp[1].split("O")[0]
+                    elif "o" in temp[1]:
+                        info["姓名"] = temp[1].split("o")[0]
+                    elif "0" in temp[1]:
+                        info["姓名"] = temp[1].split("0")[0]
+                    elif "男)" in temp[1]:
+                        info["姓名"] = temp[1].split("男)")[0]
+                    elif "侽男)" in temp[1]:
+                        info["姓名"] = temp[1].split("侽男)")[0]
+                    else:
+                        info["姓名"] = temp[1]
+                elif "注册" in temp[0]:
+                    info["注册号"] = temp[1]
+            elif "~" in word:
+                if "主册" in word:
+                    word = word.replace("主册", "注册")
+
+                register_list.append(word)
+
+        level_list = []
+        for index, item in enumerate(register_list):
+            if "级" in item:
+                level_temp = item.split("级")
+
+                if len(level_temp) == 2:
+                    if len(level_temp[0]) == 0:
+                        level_list.append(True)
+                    elif len(level_temp[0]) == 1:
+                        level_list.append(level_temp[0])
+                    else:
+                        level_list.append(False)
+                else:
+                    level_list.append(False)
+            else:
+                level_list.append(False)
+
+        if True in level_list:
+            for item in level_list:
+                if isinstance(item, str):
+                    level_temp = item
+                    return
+            for index, item in enumerate(level_list):
+                if item is True:
+                    register_list[index] = "{}{}".format(
+                        level_temp, register_list[index])
+
+        info["证书"] = "\n".join(register_list)
+
+        self.info = self.info.append(info, ignore_index=True)
+
+    def next_page(self):
+        self.win.mouse_move(335, 565)
+        hold_on(5)
+        self.win.mouse_move(600, 375)
+
+    def save_excel(self):
+        self.info.to_excel(
+            "./{}.xlsx".format(int(time.time() * 1000)), index=False)
+        self.info.to_sql()
+        del self.info
+        self.info = pandas.DataFrame(columns=["姓名", "注册号", "证书", "电话"])
 
     def auto_click(self):
-        for _ in range(10):
+        a = time.time()
+        for _ in range(85):
             self.win.mouse_move(230, 160)
-            hold_on(5)
-
+            hold_on(3)
+            self.win.mouse_move(600, 345)
+            hold_on(0.2)
             self.win.mouse_move(555, 540)
-            hold_on(2)
+            hold_on(0.5)
 
             self.get_word()
 
-        self.win.mouse_move(335, 565)
-        hold_on(5)
+        for index in range(16):
+            offset = 24 * index
+            self.win.mouse_move(230, 160 + offset)
+            hold_on(3)
+            self.get_word()
 
-        self.win.mouse_move(600, 375)
+        # self.next_page()
+        self.save_excel()
+        print("next_page")
+        print(time.time() - a)
+        hold_on(15)
 
     def main(self):
         for index in range(10):
