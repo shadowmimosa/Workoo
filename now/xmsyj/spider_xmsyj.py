c5772be7b3c97b667c076548305cffa7ccd72c1c
ShadowMimosa <chexuandong@foxmail.com>
2020-04-21 23:44:23 +0800

    complete the project

--- now/xmsyj/spider_xmsyj.py
+++ now/xmsyj/spider_xmsyj.py
@@ -12,20 +12,30 @@ class ExcelOpea(object):
         self.init_sheet(header)
         super().__init__()
 
-    def write(self, content):
+    def write(self, content, status=1):
         for index, value in enumerate(content):
-            self.write_sheet.cell(self.row, index + 1, value)
+            value = remove(value) if status else value
+            self.sheet.cell(self.row, index + 1, value)
         self.row += 1
 
     def init_sheet(self, header):
         self.wkb = init_workbook()
-        self.sheet = self.write_wkb.create_sheet('sheet1', 0)
+        self.sheet = self.wkb.create_sheet('sheet1', 0)
         self.row = 1
 
-        self.write(header)
+        self.write(header, status=0)
 
     def save(self, name):
-        self.write_wkb.save(f'{name}.xlsx')
+        self.wkb.save(f'{name}.xlsx')
+
+
+def remove(content):
+    return content.replace('比前一周', '').replace('较去年同期', '').replace(
+        '全国规模以上生猪定点屠宰企业生猪平均收购价格为',
+        '').replace('环比',
+                    '').replace('较前一周',
+                                '').replace('同比',
+                                            '').replace('白条肉平均出厂价格为', '')
 
 
 class DealXmsyj(object):
@@ -48,63 +58,55 @@ class DealXmsyj(object):
         }
         super().__init__()
 
-    def extarcting(self, data: dict):
-        if not data.get('realChannel') == '分析报告日报':
+    def detail(self, href):
+        resp = requests.get(href, headers=self.header)
+        if resp.status_code != 200:
             return
-
-        self.init_sheet()
-        title = data.get('title')
-        sign = ':' if ':' in title else '：'
-        title = title.split(sign)[-1]
-
-        # publish_time = title.split(sign)[0]
-        publish_time = data.get('publishTime').split(' ')[0]
-
-        content = BeautifulSoup(data.get('content'), "lxml").text
-
-        for field in ['猪肉', '牛肉', '羊肉', '鸡蛋', '白条鸡', '鲫鱼', '鲤鱼', '白鲢鱼', '大带鱼']:
-            info = [publish_time, field, '公斤']
-            pattern = field + r'(平均价格为)?([1-9]\d*\.\d*|0\.\d*[1-9]\d*)'
-            price = re.search(pattern, content)
-            if price:
-                info.insert(2, price.group(2))
-                self.write(info)
-
-        self.write_wkb.save('{} {}.xlsx'.format(
-            publish_time,
-            title.replace(sign, ' ').replace('"',
-                                             '').replace('”',
-                                                         '').replace('“', '')))
+        resp.encoding = 'utf-8'
+        soup = BeautifulSoup(resp.text, 'lxml')
+        return soup
 
     def info_1(self, href):
-        resp = requests.get(href, header=self.header)
-        soup = BeautifulSoup(resp, 'lxml')
+        soup = self.detail(href)
+        if not soup:
+            return
         info = []
+        week = soup.find(attrs={
+            'class': 'sj_xiang_biaoti'
+        }).text.split('周')[0] + '周'
         info.append(soup.find(attrs={'class': 'sj_dc_2'}).text.split('：')[-1])
-        temp = soup.find(attrs={'align': 'justify'}).text
+        temp = soup.find(attrs={'class': 'TRS_Editor'})
+        if not temp:
+            temp = soup.find(attrs={'id': 'TRS_AUTOADD'}).p
+        temp = temp.text
         temp = temp.split('。')
         pork = temp[0]
         meat = temp[1]
-        temp = pork.split('，')
+        temp = pork.replace(',', '，').split('，')
         info.append(temp[1])
-        info.append(temp[2].replace('全国规模以上生猪定点屠宰企业生猪平均收购价格为', ''))
-        info.append(temp[3].replace('环比', ''))
-        info.append(temp[4].replace('同比', ''))
+        info.append(temp[2])
+        info.append(temp[3])
+        info.append(temp[4])
         temp = meat.split('，')
-        info.append(temp[0].replace('白条肉平均出厂价格为', ''))
-        info.append(temp[1].replace('环比', ''))
-        info.append(temp[2].replace('同比', ''))
+        info.append(temp[0])
+        info.append(temp[1])
+        info.append(temp[2])
+        info.append(week)
 
         self.sheet1.write(info)
 
     def info_2(self, href):
-        resp = requests.get(href, header=self.header)
-        soup = BeautifulSoup(resp, 'lxml')
+        soup = self.detail(href)
+        if not soup:
+            return
         info = []
         info.append(soup.find(attrs={'class': 'sj_dc_2'}).text.split('：')[-1])
-        temps = soup.find(attrs={
-            'class': 'MsoNormalTable'
-        }).find_all(attrs={'class': 'MsoNormal'})
+        temps = soup.find(attrs={'class': 'MsoNormalTable'})
+        if not temps:
+            temps = soup.find('table').find_all(attrs={'align': 'center'})
+        else:
+            temps = soup.find('table').find_all(attrs={'class': 'MsoNormal'})
+
         info.append(temps[2].text)
         info.append(temps[3].text)
         info.append(temps[-1].text)
@@ -112,25 +114,39 @@ class DealXmsyj(object):
         self.sheet2.write(info)
 
     def info_3(self, href):
-        resp = requests.get(href, header=self.header)
-        soup = BeautifulSoup(resp, 'lxml')
-        push_time = soup.find(attrs={'class': 'sj_dc_2'}).text.split('：')[-1]
-        collect_time = soup.find(attrs={
-            'class': 'sj_xiang_biaoti'
-        }).text.replace('畜产品和饲料集贸市场价格情况', '')
+        soup = self.detail(href)
+        if not soup:
+            return
+        publish_time = soup.find(attrs={
+            'class': 'sj_dc_2'
+        }).text.split('：')[-1]
+        content = soup.find(attrs={'class': 'TRS_Editor'})
+        if not content:
+            content = soup.find(attrs={'id': 'TRS_AUTOADD'})
+        content = content.text.replace('与去年同期相比（以下简称同比）', '同比')
+        collect_time = re.search(r'[0-9]*月份.*周（采集日为[0-9]*月[0-9]*日）',
+                                 content).group()
 
-        content = soup.find(attrs={'class': 'TRS_Editor'}).text.replace('与去年同期相比（以下简称同比）','')
-        info = []
+        for field in [
+                '全国活猪', '全国猪肉', '全国仔猪', '主产省份鸡蛋', '活鸡', '白条鸡', '商品代蛋雏鸡',
+                '商品代肉雏鸡', '全国牛肉', '主产省份牛肉', '全国羊肉', '主产省份羊肉', '奶牛主产省（区）生鲜乳',
+                '全国玉米', '主产区东北三省玉米', '主销区广东省玉米', '全国豆粕', '肉鸡配合饲料', '蛋鸡配合饲料'
+        ]:
 
-        self.sheet2.write(info)
+            info = [publish_time, collect_time, field.replace('全国', '')]
+            pattern = field + r'(平均价格)?(\d+\.?\d*元/(只|公斤))[，。；]?比前一周((下降|上涨)\d+\.?\d*\%)[，。；]?同比((下降|上涨)\d+\.?\d*\%)'
+            result = re.search(pattern, content)
+            if result:
+                info.append(result.group(2))
+                info.append(result.group(4))
+                info.append(result.group(6))
+                self.sheet3.write(info)
 
     def obtaining(self, page):
         path = f'http://www.xmsyj.moa.gov.cn/jcyj/' if page == 0 else f'http://www.xmsyj.moa.gov.cn/jcyj/index_{page}.htm'
-        resp = requests.get(path, headers=self.header)
-        if resp.status_code != 200:
+        soup = self.detail(path)
+        if not soup:
             return
-        resp.encoding = 'utf-8'
-        soup = BeautifulSoup(resp.text, 'lxml')
         lis = soup.find(attrs={'id': 'div'}).find_all('li')
         hrefs = [{
             'title':
@@ -148,18 +164,27 @@ class DealXmsyj(object):
             if not data:
                 break
 
-            for title in data:
-                if '生猪及猪肉价格' in title:
-                    pass
-                elif '农村集贸市场仔猪平均价格' in title:
-                    pass
-                elif '畜产品和饲料集贸市场价格情况' in title:
-                    pass
+            for item in data:
+                if '生猪及猪肉价格' in item.get('title'):
+                    self.info_1(item.get('href'))
+                elif '农村集贸市场仔猪平均价格' in item.get('title'):
+                    self.info_2(item.get('href'))
+                elif '畜产品和饲料集贸市场价格情况' in item.get('title'):
+                    self.info_3(item.get('href'))
 
-                self.extarcting(item)
+        self.sheet1.save('生猪及猪肉价格')
+        self.sheet2.save('农村集贸市场仔猪平均价格')
+        self.sheet3.save('畜产品和饲料集贸市场价格情况')
+
+
+def main():
+    spider = DealXmsyj()
+    try:
+        spider.main()
+    except Exception as exc:
+        print(f'Something Wrong, error is {exc}')
 
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    spider = DealXmsyj()
-    spider.main()
+    main()
