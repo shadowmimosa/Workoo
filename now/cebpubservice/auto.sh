a788c664cc81bdfcdf33e427c596d6f93216328f
ShadowMimosa <chexuandong@foxmail.com>
2020-05-19 22:46:47 +0800

    remove project

--- now/cebpubservice/auto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIRPATH="/root/spider/"
-
-#cebpubservice
-source /root/.local/share/virtualenvs/cebpubservice-fFFyqeAB/bin/activate
-cd "${DIRPATH}/cebpubservice"
-nohup python run.py &
-nohup python sync.py &
--- now/cebpubservice/auto_clear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIRPATH="/root/spider/"
-
-#cebpubservice
-source /root/.local/share/virtualenvs/cebpubservice-fFFyqeAB/bin/activate
-cd "${DIRPATH}/cebpubservice"
-nohup python clear_folder.py &
