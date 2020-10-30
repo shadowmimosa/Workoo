f7073d7ec148f331a6cbe87aa8b1f9398b5396a1
ShadowMimosa <chexuandong@foxmail.com>
2020-10-30 12:26:54 +0800

    discard selenium

---
+++ now/erlangcha/common.py
@@ -0,0 +1,42 @@
+import hmac
+import time
+import base64
+import random
+import hashlib
+from urllib import parse
+
+
+def content_type(data):
+    params = parse.quote(parse.urlencode(sorted(data.items())))
+
+    hashing = hmac.new(
+        bytes('33e78d60bc1f9dcc7291c891e6f069e4&', encoding='utf-8'),
+        bytes(params, encoding='utf-8'), hashlib.sha1)
+
+    return base64.b64encode(hashing.digest()).decode('utf-8')
+
+
+def csrf_sign(data: dict):
+    sign_str = f'{data.get("Keep-Mt")}{data.get("Keep-At")}{data.get("Keep-Csrf")}'
+    sign_md5 = hashlib.md5()
+    sign_md5.update(sign_str.encode('utf-8'))
+
+    data.update({'Csrf-Sign': sign_md5.hexdigest()})
+
+    return data
+
+
+def random_header(params):
+    headers = {
+        'Keep-Mt': str(round(random.random() * 9999)),
+        'Keep-At': str(int(time.time() / 1000)),
+        'Keep-Csrf': 'cabbd66dd58ccf36c5e3119685ef39eb',
+        'X-Content-Type': 'application/json;' + content_type(params),
+        'Accept': 'application/json, text/plain, */*',
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9'
+    }
+
+    return csrf_sign(headers)
---
+++ now/erlangcha/spider_erlangcha_auto.py
@@ -0,0 +1,138 @@
+from urllib import parse
+from loguru import logger
+from fake_useragent import UserAgent
+
+from config import DEBUG
+from common import random_header
+from concurrent.futures.thread import ThreadPoolExecutor
+from utils import request, run_func, mongo
+
+# 5 首次入库
+# 6 多方云首次入库
+# 6 多方云第二次入库
+# 6 多方云第二次入库
+# 7 多方云第三次入库
+# 8 二郎查第一次入库
+UA = UserAgent()
+RUN_SIGN = 8
+
+
+def remove_charater(content: str):
+    return content.replace('\n', '').replace('\t',
+                                             '').replace('\r',
+                                                         '').replace(' ', '')
+
+
+@run_func()
+def get_shop_phone(link: str):
+    shop_id = link.split('id=')[-1]
+    uri = f'https://luban.snssdk.com/shop/info?id={shop_id}'
+    header = {
+        'User-Agent': UA.random,
+        'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9'
+    }
+    resp = request(uri, header, json=True)
+    return resp.get('data').get('company_name'), resp.get('data').get('mobile')
+
+
+@run_func()
+def get_good_phone(link):
+    good_id = link.split('id=')[-1]
+    uri = f'https://ec.snssdk.com/product/lubanajaxstaticitem?id={good_id}'
+    header = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        # 'Proxy-Authorization': made_secret()
+    }
+    resp = request(uri, header, json=True)
+
+    return resp.get('data').get('mobile')
+
+
+@run_func()
+def deal_price(_min, _max):
+
+    return '-'.join(sorted(set((str(_min), str(_max)))))
+
+
+@run_func()
+def deal_category(category: dict):
+    if not category:
+        return ''
+
+    return category.get('third_type_name')
+
+
+@run_func()
+def detail(good: dict):
+
+    info = {}
+    info['产品价格'] = deal_price(good.get('price'), good.get('price_max'))
+    # pay_type: 0 货到付款 1 在线支付 2 在线/货到
+    info['支付方式'] = good.get('pay_type')
+    info['24小时销量'] = good.get('today_volume')
+    info['3天销量'] = good.get('three_total')
+    info['7天销量'] = good.get('seven_total')
+    info['产品类别'] = deal_category(good.get('shop_type'))
+    info['产品名'] = good.get('shop_title')
+    info['产品链接'] = good.get('shop_link')
+    info['产品店铺'] = good.get('store_name')
+
+    info['抢购电话'] = get_good_phone(info['产品链接'])
+    info['公司名'], info['公司电话'] = get_shop_phone(good.get('link'))
+
+    return info
+
+
+@run_func()
+def writer(row: dict, category=None):
+    row.update({'category': f'{category}_{RUN_SIGN}'})
+    mongo.insert(row, 'boss168')
+
+
+@run_func()
+def auto_page(field, page=1):
+    params = {
+        'page': page,
+        'field': field,
+        'order_type': 'desc',
+        'dat_source_type': 1
+    }
+
+    host = 'https://www.erlangcha.com'
+    path = '/api/getShopList'
+    header = random_header(params)
+    params = parse.urlencode(params)
+    uri = parse.urljoin(host, path)
+    url = uri + '?' + params
+
+    resp = request(url, header, json=True)
+
+    for good in resp.get('data').get('data'):
+        good_detail = detail(good)
+        writer(good_detail, field)
+
+    logger.info(f'{field} - {page} 已完成')
+
+
+def main():
+    for field in ['three_total', 'seven_total', 'sales_volume']:
+        if False:
+            for page in range(1, 5010):
+                auto_page(field, page)
+        else:
+            with ThreadPoolExecutor(5) as executor:
+                pages = [x for x in range(1, 5010)]
+                fields = [field for x in range(1, 5010)]
+                executor.map(auto_page, fields, pages)
+                # executor.submit(auto_page, field, page)
+
+
+if __name__ == "__main__":
+    main()
